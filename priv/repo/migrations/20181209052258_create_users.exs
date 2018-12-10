8c64da104edf3af7ae5ae7f67423c7d634024ea8
Unknown <jarekbird@gmail.com>
2018-12-10 06:18:43 -0700

    Users N Stuff

---
+++ priv/repo/migrations/20181209052258_create_users.exs
@@ -0,0 +1,14 @@
+defmodule Thumbsup.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :phone_number, :string
+
+      timestamps()
+    end
+
+  end
+end
