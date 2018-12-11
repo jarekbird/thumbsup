430a608673225bbdef5bbcec20cb01d8e48edaa3
Unknown <jarekbird@gmail.com>
2018-12-10 17:17:51 -0700

    Send a Text with Bandwidth

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Thumbsup.Mixfile do
   def application do
     [
       mod: {Thumbsup.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :httpotion]
     ]
   end
 
@@ -40,7 +40,9 @@ defmodule Thumbsup.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:httpotion, "~> 3.1.0"},
+      {:jason, "~> 1.1"}
     ]
   end
 
